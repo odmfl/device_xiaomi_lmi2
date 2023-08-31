469dcc52cb0d3cdc5868e85a822a12786f6e401d
ahmedtohamy1 <1ahmed.tohamy@gmail.com>
2023-08-31 12:44:28 +0300

    lmi: Compile HWUI for performance * Activates a few optimizing compiler flags for HWUI. * -fno-omit-frame-pointer, -marm, -mapcs

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -81,6 +81,9 @@ DEVICE_MANIFEST_FILE := \
 DEVICE_MATRIX_FILE := \
     $(DEVICE_PATH)/configs/vintf/compatibility_matrix.xml
 
+# HWUI
+HWUI_COMPILE_FOR_PERF := true
+
 # Init
 TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):libinit_lmi
 TARGET_RECOVERY_DEVICE_MODULES := libinit_lmi
